14c40d35623ea9026e26c1c13c650702a3ccf210
Mathias Hoppe <hoppe@chalmers.se>
2019-03-15 17:59:30 +0100

    Implemented support for distribution functions from LUKE

--- include/Init/InitDistribution.h
+++ include/Init/InitDistribution.h
@@ -16,9 +16,9 @@ DistributionFunction *InitDistributionFunction(MagneticField2D*, ConfigBlock*, C
 RadialDistributionFunction *InitAvalancheDistribution(MagneticField2D*, ConfigBlock*, ConfigBlock*);
 slibreal_t InitAvalancheDistribution_param(ConfigBlock*, const std::string&);
 RadialDistributionFunction *InitCODEDistribution(MagneticField2D*, ConfigBlock*, ConfigBlock*);
-DistributionFunction *InitLUKEDistribution(ConfigBlock*);
+DistributionFunction *InitLUKEDistribution(MagneticField2D*, ConfigBlock*);
 DistributionFunction *InitNORSEDistribution(MagneticField2D*, ConfigBlock*, ConfigBlock*);
-SOFTDistributionFunction *InitNumericalDistribution(ConfigBlock*);
+SOFTDistributionFunction *InitNumericalDistribution(MagneticField2D*, ConfigBlock*);
 
 // Radial profiles
 RadialProfile *InitRadialProfile(MagneticField2D*, Setting*, ConfigBlock*, const std::string&);
