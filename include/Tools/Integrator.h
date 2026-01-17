44e18be08e8d19b8cb4594bc3cb0ae01d5201046
Mathias Hoppe <mhop@kth.se>
2026-01-17 16:18:14 +0100

    Add HDF5 output for @Integrator

--- include/Tools/Integrator.h
+++ include/Tools/Integrator.h
@@ -1,6 +1,7 @@
 #ifndef _SOFT_INTEGRATOR_H
 #define _SOFT_INTEGRATOR_H
 
+#include <string>
 #include "Tools/OutputModule.h"
 #include "Tools/Tool.h"
 
@@ -9,10 +10,12 @@ namespace __SOFT {
     private:
         slibreal_t I;
 		SOFT *soft;
+
+		std::string output;
     public:
         Integrator(SOFT*, MagneticField2D*, ParticleGenerator*, ParticlePusher*);
 
-        virtual void Configure(struct global_settings*, ConfigBlock*, ConfigBlock*) override {}
+        virtual void Configure(struct global_settings*, ConfigBlock*, ConfigBlock*) override;
         virtual void Finish() override {}
         virtual void Handle(Orbit*, Particle*) override;
         virtual void Initialize() override {}
